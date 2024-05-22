10d608f78ea558cb5e46b5926cf35b3089a6bab3
Amid-MRT <amid.mortazavi@gmail.com>
2024-05-22 15:04:18 +0330

    fix operations

--- calJS.js
+++ calJS.js
@@ -1,7 +1,8 @@
 let firstVar = '';
 let secondVar = '';
-let operator;
+let operator ='';
 let container ='';
+let containerNum;
 
 function add (a,b) {
     return a+b;
@@ -19,28 +20,51 @@ function multi (a,b) {
     return a*b;
 }
 
+// function equal (a,b) {
+//     return a==b;
+// }
+
 function operate (firstVar, secondVar, operator) {
-    return operator(firstVar, secondVar);
+    return parseInt(operator(firstVar, secondVar));
 }
 
 function clearAll () {
     display.textContent = '';
     container = '';
+    firstVar ='';
+    secondVar ='';
 }
 
 const digits = Array.from(document.querySelectorAll(".digits button"));
 const display = document.querySelector(".display");
-const ops = Array.from(document.querySelectorAll(".ops button"))
+const ops = Array.from(document.querySelectorAll(".ops .op"));
+const res = Array.from(document.querySelectorAll(".ops .res"));
 
 digits.forEach((item)=> (item.addEventListener("click", ()=> {display.textContent += item.textContent;
     container += item.textContent;
 })));
 
+res.forEach((item)=> (item.addEventListener("click", ()=> {
+    if (item.textContent == "C") {
+        clearAll();
+    } else if (item.textContent == "=") {
+        //clearAll();
+        containerNum = parseInt(container);
+        secondVar = containerNum;
+        result = operate(firstVar, secondVar, operator);
+        display.textContent = result;
+    }
+})))
+
 ops.forEach((item)=> (item.addEventListener("click", ()=> {
-    firstVar == '' ? firstVar = parseInt(container) : secondVar = parseInt(container);
-    console.log(container);
-    container ='';
-    display.textContent += item.textContent;
+    containerNum = parseInt(container);
+    ["-", "+", "*", "/"].includes(display.textContent[display.textContent.length-1]) ?
+    display.textContent[display.textContent.length-1] = item.textContent : display.textContent += item.textContent;
+    firstVar == '' ? firstVar = containerNum : secondVar = containerNum;
+    secondVar !== '' ? result = operate(firstVar, secondVar, operator) : result = firstVar;
+    firstVar = result;
+    container = '';
+    secondVar = '';
     switch (item.textContent) {
         case ("+"):
             operator = add;
@@ -54,18 +78,5 @@ ops.forEach((item)=> (item.addEventListener("click", ()=> {
         case ("/"):
             operator = division
             break;
-        case ("C"):
-            clearAll();
-            break;
-        default:
-            if (secondVar == "") {
-                display.textContent = firstVar;
-            } else {
-                result = operate(firstVar, secondVar, operator);
-                firstVar = result;
-                console.log(result);
-                return result;
-            }
-            
     }
-})))
+})));
